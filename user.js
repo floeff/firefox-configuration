130ea79b6f62cc5435c8e564524481dc38098889
Florian Effenberger <florian@effenberger.org>
2023-01-17 18:36:30 +0100

    Create user.js
    Initial version of user.js configuration

---
+++ user.js
@@ -0,0 +1,5 @@
+/* Do not remember client SSL certificates */
+user_pref("security.remember_cert_checkbox_default_setting", false);
+
+/* Disable site-specific zoom */
+user_pref("browser.zoom.siteSpecific", false);
